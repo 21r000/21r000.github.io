9a5b0e9c67d0aa32e9964e95ccf3548efd03b833
21r00 0 <82251431+2er0-b@users.noreply.github.com>
2022-03-01 19:06:28 +0800

    Site updated: 2022-03-01 19:06:27

---
+++ js/header/index.js
@@ -0,0 +1,3 @@
+// build time:Tue Mar 01 2022 19:06:26 GMT+0800 (中国标准时间)
+var o=$("#navHeader");var e=o.outerHeight();$(window).on("scroll",function(){const r=$(window).scrollTop();if(r>e){o.addClass("header-bg-color")}else{o.removeClass("header-bg-color")}});
+//rebuild by hrmmi 
